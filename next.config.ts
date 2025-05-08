911bf9c0403c716fbcf5b2c3737a6d379526ab07
Saban Yavasca <sabanyavasca@Yasemins-MacBook-Air.local>
2025-05-07 23:56:38 -0400

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/bagelicious",
+  output: "export", // <=== enables static exports
+  reactStrictMode: true,
 };
 
 export default nextConfig;
